ee97720374a92640c6f671a5691e074a1a52a37c
Kevin Nimi <k.nimi73@gmail.com>
2024-09-02 14:16:51 -0400

    creation de la navbar et ajout de kinde pour l'authentification

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,56 @@
+import Link from "next/link";
 import React from "react";
+import NavbarLinks from "./NavbarLinks";
+import { Button } from "@/components/ui/button";
+import MobileMenu from "./MobileMenu";
+// import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import UserNav from "./UserNav";
 
-function Navbar() {
+async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  console.log(user);
   return (
-    <div>
-      <p>Hello navbar</p>
-    </div>
+    <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
+      <div className="md:col-span-3">
+        <Link href="/">
+          <h1 className="text-2xl font-semibold ">
+            Market<span className="text-primary">ZO</span>
+          </h1>
+        </Link>
+      </div>
+      <NavbarLinks />
+      <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
+        {user ? (
+          <UserNav
+            email={user.email as string}
+            name={user.given_name as string}
+            userImage={
+              user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
+            }
+          />
+        ) : (
+          <div className="flex items-center gap-2">
+            <Button asChild>
+              <LoginLink postLoginRedirectURL="/">Login</LoginLink>
+            </Button>
+            <Button asChild variant="outline">
+              <RegisterLink postLoginRedirectURL="/">Register</RegisterLink>
+            </Button>
+          </div>
+        )}
+
+        <div className="md:hidden">
+          <MobileMenu />
+        </div>
+      </div>
+    </nav>
   );
 }
 
---
+++ app/sell/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SellRoute() {
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8 mb-14">
+      sell route hello
+    </section>
+  );
+}
